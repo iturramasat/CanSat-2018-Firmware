a72f8a5565936180ff84a912781da888a08bba2b
Xabi Xab <xabixab@gmail.com>
2018-03-14 12:08:45 +0100

    general rewrite. change bmp180 library to adafruit bmp085

--- iturramasat/iturramasat.ino
+++ iturramasat/iturramasat.ino
@@ -1,86 +1,13 @@
-#include <SFE_BMP180.h>
-#include <Wire.h>
+/*
+
+   GPS
+
+*/
+
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
-
 SoftwareSerial gpsSerial(2, 3); // RX, TX
 TinyGPS gps;
-
-SFE_BMP180 bmp180;
-
-double Po; //presion del punto inicial para h=0
-char status;
-double Tbmp, Pbmp, Hbmp;
-
-float T1 = 293;//Lur mailako presioa, tenperatura eta altuera
-float P1 = 96724;
-float h1 = 0;
-
-#include <SD.h>
-
-File myFile;
-
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
- 
-const int pinCE = 9;
-const int pinCSN = 10;
-RF24 radio(pinCE, pinCSN);
- 
-// Single radio pipe address for the 2 nodes to communicate.
-const uint64_t pipe = 0xA8E8F0F0E1LL;
- 
-float data[9];
-int counter = 0;
- 
-void setup()
-{
-  Serial.begin(9600);
-  gpsSerial.begin(9600);
-  if (bmp180.begin())
-  {
-    Serial.println("BMP180 piztua");
-    status = bmp180.startTemperature(); //Inicio de lectura de temperatura
-    if (status != 0)
-    {
-      delay(status); //Pausa para que finalice la lectura
-      status = bmp180.getTemperature(Tbmp);//Obtener la temperatura
-      if (status != 0)
-      {
-        status = bmp180.startPressure(3); //Inicio lectura de presión
-        if (status != 0)
-        {
-          delay(status); //Pausa para que finalice la lectura
-          status = bmp180.getPressure(Pbmp, Tbmp); //Obtener la presión
-          if (status != 0)
-          {
-            Po = Pbmp; //Asignamos el valor de presión como punto de referencia
-            Serial.println("Erreferentzia puntua: h=0");
-          }
-        }
-      }
-    }
-  }
-  else
-  {
-    Serial.println("BMP180 pizteko arazoak");
-    while (1);
-  }
-
-  Serial.print("SD pizten ...");
-  if (!SD.begin(4)) 
-  {
-    Serial.println("SD erabilarazteko arazoak");
-    return;
-  }
-  Serial.println("SD erabilarazia");//ARRAROA????????????
-
-    
-   radio.begin();
-   radio.openWritingPipe(pipe);
-
-}
 
 long lat, lon;
 unsigned long fix_age, gps_time, date, gps_speed, course;//COURSEREN KOLOREA ????????????
@@ -89,9 +16,105 @@ unsigned short sentences, failed_checksum;
 float gps_alt;
 
 
+/*
+
+   BMP180
+
+*/
+
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+Adafruit_BMP085 bmp;
+
+double Po; //presion del punto inicial para h=0
+char status;
+double Tbmp, Pbmp, Hbmp;
+
+
+/*
+
+   SD TXARTELA
+
+*/
+
+#include <SD.h>
+File myFile;
+
+
+/*
+
+    NRF24l01
+
+*/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+const int pinCE = 9;
+const int pinCSN = 10;
+RF24 radio(pinCE, pinCSN);
+
+// Single radio pipe address for the 2 nodes to communicate.
+const uint64_t pipe = 0xA8E8F0F0E1LL;
+float data[9];
+int counter = 0;
+
+/*
+
+   MPX
+
+*/
+
+float readPressure(int pin) {
+  int pressureValue = analogRead(pin);
+  float Pmpx = ((pressureValue / 1024.0) + 0.095) / 0.000009;
+  return Pmpx;
+}
+
+/*
+
+   Besteak
+
+*/
+
+float T1 = 293;//Lur mailako presioa, tenperatura eta altuera
+float P1 = 96724;
+float h1 = 0;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  gpsSerial.begin(9600);
+
+  if (!bmp.begin()) {
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
+  } else {
+    Serial.println("BMP Sentsorea piztuta");
+  }
+
+  Serial.print("SD pizten ...");
+  if (!SD.begin(4)) {
+    Serial.println("SD erabilarazteko arazoak");
+    //return;
+  } else {
+    Serial.println("SD erabilarazia");//ARRAROA????????????
+  }
+
+
+  radio.begin();
+  radio.openWritingPipe(pipe);
+
+}
+
+
+
 void loop()
-{ 
-  
+{
+  readGroundStation();
   double Hfor;
 
   if (gpsSerial.available()) {
@@ -113,61 +136,32 @@ void loop()
     }
   }
 
-  status = bmp180.startTemperature(); //Inicio de lectura de temperatura
-  if (status != 0)
-  {
-    delay(status); //Pausa para que finalice la lectura
-    status = bmp180.getTemperature(Tbmp); //Obtener la temperatura
-    if (status != 0)
-    {
-      status = bmp180.startPressure(3); //Inicio lectura de presión
-      if (status != 0)
-      {
-        delay(status); //Pausa para que finalice la lectura
-        status = bmp180.getPressure(Pbmp, Tbmp); //Obtener la presión
-        if (status != 0)
-        {
-          Hbmp = bmp180.altitude(Pbmp, Po); //Calcular altura con respecto al punto de referencia
+  Tbmp = bmp.readTemperature();
+  Pbmp = bmp.readPressure();
+  Hbmp = bmp.readAltitude();
+  
+  int value = analogRead(A3);
+  float millivolts = (value / 1023.0) * 5000;
+  float Tlm35 = millivolts / 10;
+  Serial.print("Tenperatura lm35= ");
+  Serial.print(Tlm35);
+  Serial.println(" ºC");
 
-          Serial.println();
+  float Pmpx = readPressure(A1);
+  float millibars = Pmpx / 100;
 
-          Serial.print("Presioa BMP180=");
-          Serial.print(Pbmp * 100);
-          Serial.println("Pascal");
+  Serial.print("Presioa MPX= ");
+  Serial.print(Pmpx);
+  Serial.println(" Pascal");
+  Serial.print("Presioa MPX= ");
+  Serial.print(millibars);
+  Serial.println(" milibar");
 
-          Serial.print("Tenperatura BMP180=");
-          Serial.print(Tbmp);
-          Serial.println("ºC");
-
-          Serial.print("Altuera BMP180=");
-          Serial.print(Hbmp);
-          Serial.println(" m");
-        }
-      }
-    }
-  }
-    int value = analogRead(A3);
-    float millivolts = (value / 1023.0) * 5000;
-    float Tlm35 = millivolts / 10; 
-    Serial.print("Tenperatura lm35= ");
-    Serial.print(Tlm35);
-    Serial.println(" ºC");
-    
-    float Pmpx = readPressure(A1);
-    float millibars = Pmpx/100;
- 
-    Serial.print("Presioa MPX= ");
-    Serial.print(Pmpx);
-    Serial.println(" Pascal");
-    Serial.print("Presioa MPX= ");
-    Serial.print(millibars);
-    Serial.println(" milibar");
-    
-    Hfor = (((T1/  -0.0065)*((pow(((Pmpx)/P1),(0.0065*287.06/9.81)))-1))+h1);
-    Serial.print("Altuera formula: ");
-    Serial.print(Hfor);
-    Serial.println(" m");
-    Serial.println();
+  Hfor = (((T1 /  -0.0065) * ((pow(((Pmpx) / P1), (0.0065 * 287.06 / 9.81))) - 1)) + h1);
+  Serial.print("Altuera formula: ");
+  Serial.print(Hfor);
+  Serial.println(" m");
+  Serial.println();
 
   myFile = SD.open("datalog.txt", FILE_WRITE);//abrimos  el archivo
 
@@ -196,29 +190,47 @@ void loop()
     myFile.print(gps_speed);
     myFile.print(",");
     myFile.print(gps_alt);
-    
+
     myFile.close(); //cerramos el archivo
 
   } else {
     Serial.println("Fitxategia irekitzeko arazoak");
   }
-  
-   data[0] = counter;
-   data[1] = Pbmp;
-   data[2] = Tbmp;
-   data[3] = Pmpx;
-   data[4] = Tlm35;
-   data[5] = lat;
-   data[6] = lon;
-   data[7] = gps_alt;
-   data[8] = gps_time;
-   data[9] = gps_speed;
-   
-   radio.write(data, sizeof data);
-   counter = counter + 1;
+
+  data[0] = counter;
+  data[1] = Pbmp;
+  data[2] = Tbmp;
+  data[3] = Pmpx;
+  data[4] = Tlm35;
+  data[5] = lat;
+  data[6] = lon;
+  data[7] = gps_alt;
+  data[8] = gps_time;
+  data[9] = gps_speed;
+
+  radio.write(data, sizeof data);
+  counter = counter + 1;
 }
-float readPressure(int pin)
-{
-    int pressureValue = analogRead(pin);
-    float Pmpx=((pressureValue/1024.0)+0.095)/0.000009;
-    return Pmpx;}
+
+/*
+ * Komunikazio bidirekzionala programatzen hasi..
+ */
+
+int send_delay;
+boolean enable_accel, cam1_recording, cam2_recording;
+
+void readGroundStation(){
+  if (radio.available())
+   {    
+      radio.read(data, sizeof data);
+      /*  1. send_delay = Delay between sends (int)
+       *  2. enable_accel = Accelerometer on/off (bool)
+       *  3. cam1_recording = Camara 1 status (Recording on/off)
+       *  4. cam2_recording = Camara 2 status (Recording on/off)
+       */
+      send_delay = data[0];
+      enable_accel = data[1];
+      cam1_recording = data[2];
+      cam2_recording = data[3];
+   }
+}
